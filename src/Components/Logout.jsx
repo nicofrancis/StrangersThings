da8f7c5a671c4036b5a74ba90ebaae19c1126020
nicofrancis <nicolofrancis10@gmail.com>
2022-07-13 16:10:18 -0600

    now only auth users can delete/edit posts

--- src/Components/Logout.jsx
+++ src/Components/Logout.jsx
@@ -4,5 +4,5 @@ export default function logout() {
   return <div>logout</div>;
 }
 
-// logging out is localstorage.clear, this is a function so youll have to invoke it, set token, comes from app.js and itll needs to be passed thru to logout component.
-// after that try to set up messages. stored on current user object
+// logging out is localstorage.clear, this is a function so I will have to invoke it, set token, comes from app.js and itll needs to be passed thru to logout component.
+// after that try to set up messages. stored on current user object.f
